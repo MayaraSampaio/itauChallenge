0135da9687d252d3b00b0f85332b1e75bb503550
Mayara Sampaio <mayarasampaio.dev@gmail.com>
2025-04-30 17:24:16 +0100

    feat(Services): add TransacaoService

--- src/main/java/itauchallenge/com/ItauChallenge/Service/TransacaoService.java
+++ src/main/java/itauchallenge/com/ItauChallenge/Service/TransacaoService.java
@@ -1,4 +1,28 @@
 package itauchallenge.com.ItauChallenge.Service;
 
+import itauchallenge.com.ItauChallenge.Controller.DTOs.TransacaoRequest;
+import org.springframework.stereotype.Service;
+
+import java.time.OffsetDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
 public class TransacaoService {
+
+    private final List<TransacaoRequest> transacoes = new ArrayList<>();
+
+    public void adicionarTransacao(TransacaoRequest transacao) {
+       transacoes.add(transacao);
+    }
+
+    public void limparTransacoes() {
+        transacoes.clear();
+    }
+
+   public List<TransacaoRequest> list(Integer intervaloTempo) {
+       OffsetDateTime intervalo = OffsetDateTime.now().minusSeconds(intervaloTempo);
+       return transacoes.stream().filter(transacoes -> transacoes.dataHora().isAfter(intervalo)).toList();
+
+   }
 }
